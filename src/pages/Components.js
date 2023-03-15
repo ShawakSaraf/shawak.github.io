01a733b7d72427db6061fcf4c1e1c865e7228f40
Shawak9 <shawaksaraf456@gmail.com>
2023-03-15 10:32:51 +0530

    style edits

--- src/pages/Components.js
+++ src/pages/Components.js
@@ -23,6 +23,45 @@ export function NavBar( { projetsRef, homeRef, aboutRef, ...props } )
 			behavior: 'smooth',
 		});
 	}
+
+	const menuRef = useRef(null);
+	const [ lastScrollY, setLastScrolY ] = useState(window.scrollY)
+	const [menuStyle, setMenuStyle] = useState({
+		position       : "fixed",
+		top            : "0",
+		transform      : "translateX(50vw)",
+		padding        : "20px 0 30px 0",
+		width          : "400px",
+		left           : "-200px",
+		backgroundColor: "var(--navbar-col)",
+		boxShadow      : "0 8px 20px rgba(0, 0, 0, 0.226)",
+		borderRadius   : "0 0 50px 50px",
+		zIndex         : "1",
+		transition     : "0.5s",
+	});
+
+	useEffect(() => {
+		const handleScroll = () => {
+			const scrollY = window.scrollY;
+			console.log( scrollY, lastScrollY );
+
+			if (scrollY > lastScrollY && menuStyle.top === "0") 
+			{
+				// slide up out of the screen
+				setMenuStyle((prevStyle) => ({ ...prevStyle, top: "-100px" }));
+			}
+			else if (scrollY === lastScrollY && menuStyle.top === "-100px") 
+			{
+				// slide down into the screen
+				setMenuStyle((prevStyle) => ({ ...prevStyle, top: "0" }));
+			}
+			setLastScrolY( window.scrollY );
+		};
+
+		window.addEventListener("scroll", handleScroll);
+
+		return () => window.removeEventListener("scroll", handleScroll);
+	}, [menuStyle]);
   
 	return (
 		<nav className="nav" role={"navigation"}>
@@ -57,7 +96,9 @@ export function About({ aboutRef })
       <div id="About" ref={aboutRef}>
          <header>About</header>
          <p>
-            Hey! I'm a human trying to figure out life.
+            Hey! I'm a human trying to figure out life.<br />
+				In the real world I'm a self-taught programmer and game developer.<br />
+				I'm also really facsinated by machine learning.
          </p>
       </div>
    );
